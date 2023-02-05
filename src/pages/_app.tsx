c01a0edfeec40f4bd5b635354d7526f06be15e81
devmaggioni <maggionigab@gmail.com>
2023-02-05 19:46:09 -0300

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
-import { Theme, Header, Footer, Menu } from "../components"
+import { Theme, Seo, Header, Footer, Menu } from "../components"
 
 const App = ({ Component, pageProps }) => {
    return (
 <>
+<Seo
+title="Gabriel Maggioni - Web Developer"
+url="https://devmaggioni.vercel.app"
+description="Meu portfólio 😝"
+image="/images/thumb.webp"
+keywords="dev, desenvolvedor, programador, desenvolvedor web, gabriel maggioni, web developer, desenvolvedor web, portfólio, currículo"
+/>
 <Header/>
 <Menu/>
 <Theme>
