adfb47ae1d7f39c0252d52772d5bb16cb4a8243e
devmaggioni <maggionigab@gmail.com>
2023-02-15 15:11:52 -0300

    fix

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,9 +4,7 @@ import { useRouter } from 'next/router'
 const Component = () => {
    const router = useRouter()
    const redirect = (url) => {
-      router.push(url);
-      document.getElementById("Menu").style.display = "none"
-      document.getElementsByTagName('body')[0].style.overflowY = 'scroll'
+      location.href = url
    }
 
    return (
